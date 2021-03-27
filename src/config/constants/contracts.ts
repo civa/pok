6c6846a3c48b21a7e7e77fb25cf8f1490dca7a64
root <root@vmi541303.contaboserver.net>
2021-03-27 16:46:12 -0500

    upload

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,10 +1,10 @@
 export default {
   cake: {
-    56: '0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
+    56: '0x8ad065b621735b45ec7386335fdf55a4005ec6e0',
     97: '',
   },
   masterChef: {
-    56: '0xe70E9185F5ea7Ba3C5d63705784D8563017f2E57',
+    56: '0x3C8da6201e9A179f18d681dB5aa68DBca6A7e3B4',
     97: '',
   },
   wbnb: {
