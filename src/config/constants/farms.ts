59ee65a5df3c1b16d2be98245f65a6eab397a8a6
Civa <44102586+civa@users.noreply.github.com>
2021-04-09 06:57:45 +0100

    farm update

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -19,14 +19,14 @@ const farms: FarmConfig[] = [
     quoteTokenAdresses: contracts.wbnb,
   },
   {
-    pid: 2,
+    pid: 1,
     risk: 5,
-    lpSymbol: 'BSCTRUST-BUSD POOL',
+    lpSymbol: 'BNB-BUSD LP',
     lpAddresses: {
       97: '',
       56: '0x1b96b92314c44b159149f7e0303511fb2fc4774f',
     },
-    tokenSymbol: 'BSCTRUST',
+    tokenSymbol: 'BNB',
     tokenAddresses: {
       97: '',
       56: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c',
@@ -35,14 +35,14 @@ const farms: FarmConfig[] = [
     quoteTokenAdresses: contracts.busd,
   },
   {
-    pid: 1,
+    pid: 2,
     risk: 3,
-    lpSymbol: 'DRYCAKE-BUSD POOL',
+    lpSymbol: 'USDT-BUSD LP',
     lpAddresses: {
       97: '',
       56: '0xc15fa3e22c912a276550f3e5fe3b0deb87b55acd',
     },
-    tokenSymbol: 'DRYCAKE',
+    tokenSymbol: 'USDT',
     tokenAddresses: {
       97: '',
       56: '0x55d398326f99059ff775485246999027b3197955',
