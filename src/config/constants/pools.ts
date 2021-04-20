d7d0078fd6bfab543a851e1b8624c8a4b91add1a
Civa <44102586+civa@users.noreply.github.com>
2021-04-20 13:28:21 +0100

    Update pools.ts

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -18,23 +18,23 @@ const pools: PoolConfig[] = [
    isFinished: false,
    tokenDecimals: 18,
  },
-  // {
-  //   sousId: 1,
-  //   tokenName: 'TWT',
-  //   stakingTokenName: QuoteToken.SYRUP,
-  //   stakingTokenAddress: '0x009cF7bC57584b7998236eff51b98A168DceA9B0',
-  //   contractAddress: {
-  //     97: '0xAfd61Dc94f11A70Ae110dC0E0F2061Af5633061A',
-  //     56: '0xAfd61Dc94f11A70Ae110dC0E0F2061Af5633061A',
-  //   },
-  //   poolCategory: PoolCategory.CORE,
-  //   projectLink: 'https://trustwallet.com/',
-  //   harvest: true,
-  //   tokenPerBlock: '20',
-  //   sortOrder: 999,
-  //   isFinished: true,
-  //   tokenDecimals: 18,
-  // },
+ {
+   sousId: 1,
+   tokenName: 'TWT',
+   stakingTokenName: QuoteToken.SYRUP,
+   stakingTokenAddress: '0x009cF7bC57584b7998236eff51b98A168DceA9B0',
+   contractAddress: {
+     97: '0xAfd61Dc94f11A70Ae110dC0E0F2061Af5633061A',
+     56: '0xAfd61Dc94f11A70Ae110dC0E0F2061Af5633061A',
+   },
+   poolCategory: PoolCategory.CORE,
+   projectLink: 'https://trustwallet.com/',
+   harvest: true,
+   tokenPerBlock: '20',
+   sortOrder: 999,
+   isFinished: true,
+   tokenDecimals: 18,
+ },
 ]
 
 export default pools
