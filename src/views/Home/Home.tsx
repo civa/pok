fa1cb4547aec0098cc42975a46c27625276a2bd6
Civa <44102586+civa@users.noreply.github.com>
2021-04-20 11:25:36 +0100

    update

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -58,7 +58,7 @@ const Home: React.FC = () => {
     <Page>
       <Hero>
         <Heading as="h1" size="xl" mb="24px" color="secondary">
-          {TranslateString(576, 'DRYCAKE Finance')}
+          {TranslateString(576, '24K Finance')}
         </Heading>
         <Text>{TranslateString(578, 'Top 3 best DEFI app on Binance Smart Chain.')}</Text>
       </Hero>
