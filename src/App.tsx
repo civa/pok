323955d044dedf91ee008c644db29df1508c5aad
Civa <44102586+civa@users.noreply.github.com>
2021-04-21 14:53:16 +0100

    d

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ const App: React.FC = () => {
             <Route path="/farms">
               <Farms />
             </Route>
-            <Route path="/poolss">
+            <Route path="/poolsd">
               <Farms tokenMode />
             </Route>
             {/* <Route path="/pools"> */}
