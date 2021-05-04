8f29409f80f548e3b3e61e73efdfe310c2a107bb
Civa <44102586+civa@users.noreply.github.com>
2021-05-04 19:25:09 +0100

    Update pools.ts

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -2,7 +2,7 @@ import { PoolConfig, QuoteToken, PoolCategory } from './types'
 
 const pools: PoolConfig[] = [
   {
-    sousId: 3,
+    sousId: 0,
     tokenName: 'POK',
     stakingTokenName: QuoteToken.POK,
     stakingTokenAddress: '0x3b7c8ca664ab6eb8ca8f424851e38b7182b36e5f',
@@ -11,7 +11,7 @@ const pools: PoolConfig[] = [
       56: '0x3195a1617C70AC263ab19Cf9a372a168AffB9EB0',
     },
     poolCategory: PoolCategory.CORE,
-    projectLink: 'https://pokswap.com/',
+    projectLink: 'https://pokswap.finance/',
     harvest: true,
     tokenPerBlock: '10',
     sortOrder: 1,
